c702154ee7d27034d0e5fdb15217d885c38a19b3
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-23 14:36:09 -0800

    Task 0: Write an algorithm in C that implements the bubble sort technique

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+* bubble_sort - A sorting algorithm implementing bubble sort
+* @array: "int *" The unsorted array to be sorted in-place
+* @size: "size_t" Size of the array
+*
+* Return: "void" Nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swapped = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+		{
+			break;
+		}
+	}
+}
+
